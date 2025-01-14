d963b58c042d64cf061f10c0d1053afd5f70fb6f
IL-aina-Ratefinanahary <ratefilaina@gmail.com>
2025-01-13 21:02:50 -0500

    configuration du portfolio

---
+++ pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Bienvenue sur mon portfolio</h1>
+      <p>Je suis Il'aina Ratefinanahary, un étudiant de baccalauréat en génie informatique.</p>
+    </div>
+  );
+}
+
+export default Home;
